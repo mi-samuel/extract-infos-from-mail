31a6b82575f4a69886d690e13d323856071917a7
Samuel JEAN <samuel@maianoinfo.com>
2024-03-12 11:23:46 +0100

    update user interface

--- extract_infos_from_mail/main.py
+++ extract_infos_from_mail/main.py
@@ -1,21 +1,8 @@
-from extract_infos_from_mail.utils.select_directory import select_directory
-from extract_infos_from_mail.utils.select_excel_file import select_excel_file
-from extract_infos_from_mail.utils.read_msg_files import read_msg_files
-from extract_infos_from_mail.utils.write_to_excel import write_to_excel
+from extract_infos_from_mail.utils.open_directory_dialog import open_directory_dialog
 
 
 def main():
-    directory = select_directory()
-    if not directory:
-        print("No directory selected. Exiting...\n")
-        return
-    excel_file_path = select_excel_file()
-    if not excel_file_path:
-        print("No Excel file selected. Exiting...\n")
-        return
-    excel_data = read_msg_files(directory)
-    write_to_excel(excel_data, excel_file_path)
-    print("Excel file created successfully.\n")
+    open_directory_dialog()
 
 
 if __name__ == "__main__":
